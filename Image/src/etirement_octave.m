f14e0e18eb0c50b9d418439220f96b3950344aa1
RenardBOT <pevan.ers@gmail.com>
2023-02-18 18:55:26 +0100

    Ajout étirement et spécification d'histogramme

--- Image/src/etirement_octave.m
+++ Image/src/etirement_octave.m
@@ -1,34 +1,41 @@
-function [Iout] = egalisation_octave(path)
+function [Iout] = etirement_octave(path)
 % ETIREMENT étire l'histogramme sur la plage [0;255]
 % Possible d'affecter la nouvelle image à une variable
 % en appelant par exemple I = etirement(chemin)
 
 % Lecture de l'image
-I = (imread(path));
+I = imread(path);
 
-% Si l'image est en couleur, on la convertit en niveaux de gris
 if ndims(I) == 3
     I = rgb2gray(I);
 end
 
+% Identification des niveaux de gris maximum et minimum
+Imax = max(max(I));
+Imin = min(min(I));
 
-% Histogramme cumulé normalisé de l'image originale
-[counts, edges]=histc(I(:), 0:255);
-IHCN = cumsum(counts) / sum(counts);
+% Calcul de l'étirement dans la matrice sortie Iout 
+Iout = uint8(floor(255*double((I-Imin))/double(Imax-Imin)));
 
-% Calcul de l'égalisation dans la matrice sortie Iout 
+% LOOKUP TABLE
+% Ordonee a l'origine
+alpha = -255*double(Imin)/double(Imax-Imin);
+% Coefficient directeur 
+beta = double(255/double(Imax - Imin));
+x = linspace(0, 255, 2551);
 
-[height,width] = size(I);
-Iout = uint8(zeros([height,width]));
-for y = 1:height
-    for x = 1:width
-        Iout(y,x) = uint8(floor(255*IHCN(I(y,x)+1)));
-    end
-end
+% Definition de la LUT par morceaux
+morceau1 = (x>=0 & x<Imin);
+morceau2 = (x>=Imin & x < Imax);
+morceau3 = (x>=Imax);
+y(morceau1) = 0;
+y(morceau2) = beta*x(morceau2)+alpha;
+y(morceau3) = 255;
 
-% Histogramme cumulé normalisé de l'image de sortie
+% Calcul de l'histogramme cumulé normalisé de l'image de sortie
 [countsOut, edgesOut]=histc(Iout(:), 0:255);
 IHCNout = cumsum(countsOut) / sum(countsOut);
+
 
 % Phase d'affichage
 f = figure;
@@ -39,7 +46,7 @@ pos(3) = 1000;
 pos(4) = 900; 
 set(f, 'Position', pos);
 
-% Affichage de l'image originale et de l'image égalisée
+% Affichage de l'image originale et de l'image étirée
 subplot(3,2,1);
 imshow(I);
 title('Image entrée');
@@ -47,7 +54,7 @@ subplot(3,2,2);
 imshow(Iout);
 title('Image sortie');
 
-% Affichage de l'histograme de l'image originale et de l'image égalisée
+% Affichage de l'histograme de l'image originale et de l'image étirée
 subplot(3,2,3);
 [countsIn, edgesIn] = hist(I(:), 0:255);
 bar(edgesIn, countsIn, "hist");
@@ -58,12 +65,17 @@ bar(edgesOut, countsOut, "hist");
 title('Histogramme image sortie');
 
 % Affichage de l'histogramme cumulé de l'image originale et de l'image
-% égalisée
+% étirée
 subplot(3,2,5);
-bar(0:255, IHCN);
-title('HCN image entrée');
+plot(x,y,'LineWidth', 2);
+xlim([0,255]);
+ylim([0,255]);
+##hold on
+title(strcat('LUT(ax+b) b : ',num2str(alpha),' - a : ',num2str(beta)));
 subplot(3,2,6);
 bar(0:255, IHCNout);
 title('HCN image sortie');
+
+
 
 end
