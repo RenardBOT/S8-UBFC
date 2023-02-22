743ebee060b4e5123f79143ed8bf414ff2d6ba03
RenardBOT <pevan.ers@gmail.com>
2023-02-22 16:37:31 +0100

    Ajout triggers

--- SIR/Scripts.sql
+++ SIR/Scripts.sql
@@ -24,7 +24,7 @@ CREATE TABLE produit(
       NomP VARCHAR(20),
       PrixHT NUMBER,
       Taux CHAR(1),
-      Nb NUMBER,
+      NbStock NUMBER,
       CONSTRAINT pk_produit_np PRIMARY KEY(NP)
 );
 
@@ -42,3 +42,44 @@ FOREIGN KEY (Taux) REFERENCES tva(Taux);
 ALTER TABLE achat1
 ADD CONSTRAINT fk_np_achat1_produit
 FOREIGN KEY (NP) REFERENCES produit(NP); 
+
+CREATE VIEW v_tva AS 
+SELECT * 
+FROM tva
+UNION
+SELECT *
+FROM tva@dbalbra;
+
+CREATE VIEW v_produit AS 
+SELECT * 
+FROM produit
+UNION
+SELECT *
+FROM produit@ens0907;
+
+
+-- SEQUENCES POUR ID
+
+CREATE SEQUENCE produit_seq START WITH 1;
+CREATE SEQUENCE achat_seq START WITH 1;
+
+
+-- TRIGGER FHD PRODUIT
+
+CREATE OR REPLACE TRIGGER new_produit_t
+      INSTEAD OF INSERT ON v_produit
+      FOR EACH ROW
+DECLARE
+      var_seq INTEGER := produit_seq.nextval;
+BEGIN
+      -- inserer dans produit@renardbot si le taux de TVA est N ou P
+      IF :NEW.Taux = 'N' OR :NEW.Taux = 'P' THEN
+            INSERT INTO produit VALUES(var_seq,:NEW.NomP,:NEW.PrixHT,:NEW.Taux,:NEW.NbStock);
+      END IF;
+      
+      -- inserer dans produit@dbalbra si le taux de TVA est I ou R
+      IF :NEW.Taux = 'I' OR :NEW.Taux = 'R' THEN
+            INSERT INTO produit@ens0907 VALUES(var_seq,:NEW.NomP,:NEW.PrixHT,:NEW.Taux,:NEW.NbStock);
+      END IF;
+END;
+/
